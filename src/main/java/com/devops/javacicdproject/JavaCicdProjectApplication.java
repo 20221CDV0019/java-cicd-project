7d2599b267ee190d564d1b58216e394fd0466768
Archi <Archi@gmail.com>
2026-08-07 22:59:37 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/devops/javacicdproject/JavaCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devops.javacicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCicdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/javacicdproject/JavaCicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.javacicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
