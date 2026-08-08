dcf63a1521499b399d7906b2244f599f4950a3c9
Archi <Archi@gmail.com>
2026-08-08 23:02:41 +0530

    Test Jenkins automatic build

--- src/main/java/com/devops/javacicdproject/HelloController.java
+++ src/main/java/com/devops/javacicdproject/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello DevOps! Spring Boot is running successfully.";
+        return "Hello DevOps! Spring Boot is running successfully - CI/CD works!";
     }
 }
