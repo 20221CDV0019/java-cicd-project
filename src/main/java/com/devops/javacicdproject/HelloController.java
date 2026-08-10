0dd331f87b921e7d3c5a1a0efa89917d2096b48e
Archi <Archi@gmail.com>
2026-08-10 21:50:06 +0530

    Update application message

--- src/main/java/com/devops/javacicdproject/HelloController.java
+++ src/main/java/com/devops/javacicdproject/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello DevOps! Jenkins automatic build is working!";
+        return "Hello DevOps! CI/CD Pipeline is working perfectly!";
     }
 }
